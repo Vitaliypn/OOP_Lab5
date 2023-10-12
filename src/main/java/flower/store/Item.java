b99a3f68cd82cc9f915f304a0dd1ee823881bea0
vitaliypn <paliichu.pn@ucu.edu.ua>
2023-10-12 12:13:47 +0300

    commit

---
+++ src/main/java/flower/store/Item.java
@@ -0,0 +1,41 @@
+package flower.store;
+
+public class Item {
+    private String name;
+    private String description;
+
+    public Item() {
+        // Default constructor with default values
+        this.name = "";
+        this.description = "";
+    }
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
